4a451bcfe43dfd8e5e10c8c7df1b351b7cd5bcea
Richard Applebaum <dicklacara@mac.com>
2016-08-21 08:49:08 -0700

    First commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        
+     coolLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+      }
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        coolLabel.text = "poopy"
     }
 
     override func didReceiveMemoryWarning() {
